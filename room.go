dc4328e66c3803ad3e174bab39a95dc6fd8ce8f0
Brandon0172 <brandonvanwijk@gmail.com>
2017-05-04 14:22:31 +0200

    add room struct

---
+++ room.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type Room struct {
+	name         string
+	size         int
+	MAX_CAPACITY int
+	occupation   int
+	hubs         []int //dit worden hubs. weet even niet hoe ik dat aangeef.
+}
+
+func main() {
+	fmt.Println("Room class")
+}
