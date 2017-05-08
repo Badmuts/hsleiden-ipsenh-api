6a88a7f80c03f5ee093ade5b45b69688f5479850
Brandon0172 <brandonvanwijk@gmail.com>
2017-05-08 21:41:20 +0200

    add Test prefix to test method

--- test_api/room_test.go
+++ test_api/room_test.go
@@ -5,8 +5,15 @@ import (
 	"testing"
 )
 
-func CheckIfRoomIsOverOccpupied(t *testing.T) {
-	room := Room{Name: "G3.076", Occupation: 20, MAX_CAPACITY: 15, Size: 20}
+type Room struct {
+	Name         string `json:"name"`
+	Size         int    `json:"size"`
+	MAX_CAPACITY int    `json:"max_capacity"`
+	Occupation   int    `json:"occupation"`
+}
+
+func TestCheckIfRoomIsOverOccpupied(t *testing.T) {
+	room := Room{Name: "G3.076", Occupation: 20, MAX_CAPACITY: 30, Size: 20}
 
 	if room.Occupation > room.MAX_CAPACITY {
 		fmt.Println("Name: ", room.Name, "Occupation: ", room.Occupation, "Max Capacity: ", room.MAX_CAPACITY)
