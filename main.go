92984cd1c9acb4f9fc8f7506d3399b463e9e3de6
Daan Rosbergen <d.rosbergen@gmail.com>
2017-05-08 23:00:20 +0200

    Setup server

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"time"
+
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+	graceful "gopkg.in/tylerb/graceful.v1"
 )
 
 func main() {
-	fmt.Println("hsleiden-ipsenh-api")
+	r := render.New()
+	router := mux.NewRouter()
+
+	router.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		r.JSON(w, http.StatusOK, map[string]bool{"healthy": true})
+	})
+
+	server := negroni.Classic()
+	server.UseHandler(router)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Starting server on: :" + port)
+	graceful.Run(":"+port, 10*time.Second, server)
 }
