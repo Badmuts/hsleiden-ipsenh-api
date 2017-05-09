7e2f15234477009a6e90f9cbc1f2286ff16dec75
Daan Rosbergen <d.rosbergen@gmail.com>
2017-05-09 12:59:46 +0200

    Refactor webserver to use different packages

---
+++ controller/health.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
+
+// HealthController represents the controller instance
+type HealthController struct {
+	router *mux.Router
+	r      *render.Render
+}
+
+// NewHealthController creates the controller
+func NewHealthController(router *mux.Router, r *render.Render) *HealthController {
+	ctrl := &HealthController{router, r}
+	ctrl.Register()
+	return ctrl
+}
+
+// Register registers the routes with mux.Router
+func (c *HealthController) Register() {
+	c.router.HandleFunc("/healthz", c.healthz)
+}
+
+func (c *HealthController) healthz(res http.ResponseWriter, req *http.Request) {
+	c.r.JSON(res, http.StatusOK, map[string]bool{"healthy": true})
+}
--- main.go
+++ main.go
@@ -2,27 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 
 	"os"
 
-	"github.com/gorilla/mux"
-	"github.com/unrolled/render"
-	"github.com/urfave/negroni"
+	"github.com/badmuts/hsleiden-ipsenh-api/web"
 	graceful "gopkg.in/tylerb/graceful.v1"
 )
 
 func main() {
-	r := render.New()
-	router := mux.NewRouter()
-
-	router.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		r.JSON(w, http.StatusOK, map[string]bool{"healthy": true})
-	})
-
-	server := negroni.Classic()
-	server.UseHandler(router)
+	server := web.NewServer()
 
 	port := os.Getenv("PORT")
 	if port == "" {
