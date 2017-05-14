93499e16381ea634ace073b0d64b67b48c3ceeb0
Brandon0172 <brandonvanwijk@gmail.com>
2017-05-14 12:18:38 +0200

    add methods to sensorType interface and DatapointModel to access db in datapoint.go

--- model/datapoint.go
+++ model/datapoint.go
@@ -1,8 +1,15 @@
 package model
 
+import (
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
 type Datapoint struct {
-	Sensor    *Sensor `json:"sensor"`
-	Key       int     `json:"key"`
-	Value     float32 `json:"value"`
-	Timestamp int64   `json:"timestamp"`
+	Sensor    *Sensor         `json:"sensor"`
+	SensorIDS []bson.ObjectId `json:"-" bson:"sensors"`
+	Key       int             `json:"key"`
+	Value     float32         `json:"value"`
+	Timestamp int64           `json:"timestamp"`
+	db        *mgo.Database
 }
