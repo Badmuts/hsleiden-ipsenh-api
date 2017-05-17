16cc353901f1664666d03c4ee9e7f2a30d421398
Brandon0172 <brandonvanwijk@gmail.com>
2017-05-17 15:06:11 +0200

    add test code

--- model/occupation_sensor_test.go
+++ model/occupation_sensor_test.go
@@ -1,16 +1,78 @@
 package model
 
 import (
+	"log"
 	"testing"
 
-	"log"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func TestOccupationSensor(t *testing.T) {
+func SetOccupationSensor() *OccupationSensor {
+	occupationSensor := &OccupationSensor{}
 
-	i := 0
-	for i < 100 {
-		log.Printf("Number %s", i)
+	occupationSensor.InSensorDatapoints = CreateDummyDatapointsIn()
+	occupationSensor.OutSensorDatapoints = CreateDummyDatapointsOut()
+	occupationSensor.TotalEntrances = 0
+	occupationSensor.TotalExits = 0
+	occupationSensor.CurrentOccupants = 0
+
+	return occupationSensor
+
+}
+
+func CalculateOccupation(occupationSensor *OccupationSensor) {
+	total_exits := occupationSensor.CalculateExits()
+
+	log.Printf("Total exits: %s", total_exits)
+}
+
+func CreateDummyDatapointsIn() []*Datapoint {
+	datapointsIn := []*Datapoint{}
+
+	number := 60.0
+	index := 1
+	for index < 5000 {
+		datapoint := &Datapoint{}
+		datapoint.ID = bson.NewObjectId()
+		datapoint.Key = "distance"
+
+		if index >= 1000 && index <= 1050 {
+			datapoint.Value = 10.0
+		} else {
+			datapoint.Value = number
+		}
+
+		datapointsIn = append(datapointsIn, datapoint)
+		index++
 	}
 
+	return datapointsIn
+}
+
+func CreateDummyDatapointsOut() []*Datapoint {
+	datapointsOut := []*Datapoint{}
+
+	number := 60.0
+	index := 1
+	for index < 5000 {
+		datapoint := &Datapoint{}
+		datapoint.ID = bson.NewObjectId()
+		datapoint.Key = "distance"
+
+		if index >= 1030 && index <= 1050 {
+			datapoint.Value = 10.0
+		} else {
+			datapoint.Value = number
+		}
+
+		datapointsOut = append(datapointsOut, datapoint)
+		index++
+	}
+
+	return datapointsOut
+}
+
+func TestOccupationSensor(t *testing.T) {
+	sensor := SetOccupationSensor()
+	CalculateOccupation(sensor)
 }
